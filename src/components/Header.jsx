b35006bb969b6cd0bc226ce351f9423926fab0fc
Infamous003 <arfathsyed0312@gmail.com>
2025-07-01 17:13:04 +0530

    Add header component and tui styling to it

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+function Header() {
+    return (<>
+        <header box-="square" shear-="top">
+            <div className="heading">
+                <span>MaBlog</span>
+            </div>
+            
+            <nav>
+                <ul marker-="open tree">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">My Blogs</a></li>
+                    <li><a href="#">Write</a></li>
+                    <li><a href="#">Logout/Login</a></li>
+                </ul>
+            </nav>
+        </header>
+    </>)
+}
+
+export default Header;
