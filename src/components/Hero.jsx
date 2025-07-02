95a440aeff436a75965110adacf5eae9100dbee7
Infamous003 <arfathsyed0312@gmail.com>
2025-07-02 16:48:20 +0530

    Add some CSS styling to hero section and user meta section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 export default function Hero() {
     return(<>
-        <pre size-="large"><b>&gt; Welcome to MaBlog</b>
+        <pre className="hero-section" size-="large"><b>&gt; Welcome to MaBlog</b>
+            <br />
             <p>&gt; A place where you can write about tech and nerdy stuff I guess</p>
         </pre>
     </>)
