21a2c5206035c514c4946127a7bb5eaba99107ee
Infamous003 <arfathsyed0312@gmail.com>
2025-07-16 16:18:32 +0530

    Remove hardcoded date and use api returned date

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -12,7 +12,7 @@ export default function Blog({ title, subtitle, date, content, username }) {
             <div className="blog-meta-data">
                 <h1 className="blog-title">{ title }</h1>
                 <h4 className="blog-subtitle">{ subtitle }</h4>
-                <p className="blog-username"><span className="username blog-username">@{ username }</span>, Dec 2023</p>
+                <p className="blog-username"><span className="username blog-username">@{ username }</span>, { date }</p>
             </div>
             <hr className="divider" />
             <p className="blog-content">
