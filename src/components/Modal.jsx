7ea819aa4f85bedff808237e88f767248774198b
Infamous003 <arfathsyed0312@gmail.com>
2025-07-20 13:08:21 +0530

    Add more styling to modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,17 @@
 export default function Modal({ onConfirm }) {
     return(<>
-        <dialog id="dialog">
+        <dialog id="dialog" className="dialog">
             <div box-="square" id="content">
                 <p>Are you sure you want to delete this blog?</p>
 
-                <div id="buttons">
-                    <button onClick={() => document.querySelector("#dialog").close()} box-="square">Cancel</button>
-                    <button onClick={onConfirm} box-="square">Delete</button>
+                <div className="btns-container">
+                    <button onClick={() => document.querySelector("#dialog").close()}
+                            className="dialog-cancel-btn"
+                            size-="small">Cancel</button>
+                    <button onClick={onConfirm}
+                            // box-="square"
+                            size-="small"
+                            className="dialog-delete-btn">Delete</button>
                 </div>
             </div>
         </dialog>
